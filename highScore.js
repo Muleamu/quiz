42ec8774235a37f8b81a7b2077cf278cce7182a9
Muleamu <113890200+Muleamu@users.noreply.github.com>
2022-10-06 20:36:16 -0700

    delete.highscore.js

--- highScore.js
+++
@@ -1,7 +0,0 @@
-const highScoreList = document.getElementById("highScoreList");
-const highScore = JSON.parse(localStorage.getItem("highScore")) // [];
-highScoreList.innerHTML = highScores 
-.map(score => {
-    return <li class="high-score"> - ${score.name}</li> ;
-})
-.join("");
